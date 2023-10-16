254d1d7defd3b13f0caa82c6c7840ed1bee40c9c
Helio Nogueira <424582+helionogueir@users.noreply.github.com>
2023-10-16 14:41:20 +0100

    Longest Common Prefix

---
+++ javascript/LongestCommonPrefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+
+    let i = 0;
+    let prefix = strs[0];
+
+    do {
+        for (let s of strs)
+            if (i >= s.length || s[i] !== prefix[i])
+                return prefix.substring(0, i);
+    } while (i++ < prefix.length)
+
+    return prefix;
+};

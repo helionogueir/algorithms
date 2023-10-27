397e4fa455433fd4fbbf834f327f78f7c776967f
Helio Nogueira <424582+helionogueir@users.noreply.github.com>
2023-10-27 10:30:02 +0100

    Binary Tree Inorder Traversal

---
+++ javascript/BinaryTreeInorderTraversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if (!root) return [];
+    
+    const result = [];
+    
+    const inorder = (node) => {
+        if (node.left) inorder(node.left);
+        result.push(node.val);
+        if (node.right) inorder(node.right);
+    };
+    
+    inorder(root);
+    
+    return result;
+};

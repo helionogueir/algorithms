d4925cb40791af89f53801c5390295f90752c772
Helio Nogueira <424582+helionogueir@users.noreply.github.com>
2024-01-29 08:56:43 +0000

    Palindrome Linked List

---
+++ javascript/PalindromeLinkedList.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let aux = head;
+    let word = '';
+
+    while (aux !== null) {
+        word += aux.val;
+        aux = aux.next;
+    }
+
+    const reverseWord = word.split('').reverse().join('');
+
+    return word === reverseWord;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let slow = head, fast = head, prev, temp
+    while (fast && fast.next)
+        slow = slow.next, fast = fast.next.next
+    prev = slow, slow = slow.next, prev.next = null
+    while (slow)
+        temp = slow.next, slow.next = prev, prev = slow, slow = temp
+    fast = head, slow = prev
+    while (slow)
+        if (fast.val !== slow.val) return false
+        else fast = fast.next, slow = slow.next
+    return true
+};
+

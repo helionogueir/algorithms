f411db659750c6f828d6df0c10d193da9f318222
Helio Nogueira <424582+helionogueir@users.noreply.github.com>
2024-02-01 09:04:41 +0000

    Move Zeroes

---
+++ javascript/MoveZeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    if (nums.length === 1) return nums;
+
+    for (i = nums.length; i >= 0; i--){
+        if (nums[i] === 0) {
+            nums.push(0);
+            nums.splice(i, 1);
+        }
+    }
+
+    return nums;
+};
